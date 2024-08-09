e02b7bc52d715a7d91ee94bfb98962bc8f8c6d48
Sylar9613 <ariancastellanosrodriguez@gmail.com>
2024-08-09 18:30:59 +0200

    Creando las relaciones entre Movie, Genre y Country y su migration

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GenreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GenreRepository::class)]
@@ -15,6 +17,17 @@ class Genre
 
     #[ORM\Column(length: 120)]
     private ?string $name = null;
+
+    /**
+     * @var Collection<int, Movie>
+     */
+    #[ORM\OneToMany(targetEntity: Movie::class, mappedBy: 'genre')]
+    private Collection $movies;
+
+    public function __construct()
+    {
+        $this->movies = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class Genre
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Movie>
+     */
+    public function getMovies(): Collection
+    {
+        return $this->movies;
+    }
+
+    public function addMovie(Movie $movie): static
+    {
+        if (!$this->movies->contains($movie)) {
+            $this->movies->add($movie);
+            $movie->setGenre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMovie(Movie $movie): static
+    {
+        if ($this->movies->removeElement($movie)) {
+            // set the owning side to null (unless already changed)
+            if ($movie->getGenre() === $this) {
+                $movie->setGenre(null);
+            }
+        }
 
         return $this;
     }

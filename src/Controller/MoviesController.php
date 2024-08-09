7b6168bd1dbd9bf4cfc291d49d3914475b467d53
Sylar9613 <ariancastellanosrodriguez@gmail.com>
2024-08-09 19:52:42 +0200

    Configurando los repositorios en MovieController

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MoviesController extends AbstractController
 {
+    public function __construct(private readonly EntityManagerInterface $entityManager) {
+        
+    }
+
     #[Route('/movies', name: 'app_movies', methods: ['GET','HEAD'])]
     public function index(): Response
     {
+        $movies = $this->entityManager->getRepository(Movie::class)->findAll();
+        dd($movies);
         return $this->render('movies/index.html.twig');
     }
 }
